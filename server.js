6425bc62bc20072844e18313281abfed1896e95b
Mor Revah <morrevah10@gmail.com>
2024-06-22 01:33:15 +0300

    fix deploy

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  tls: true,
+  tlsAllowInvalidCertificates: false
 });
 
 const ProductSchema = new mongoose.Schema({
